201098bf4daab9be3f2b9165cd13954776cee1aa
David110326 <975298420@qq.com>
2023-02-23 17:51:14 +0800

    添加了 open_browaer_2

---
+++ Baidu/open_browser_2.py
@@ -0,0 +1,9 @@
+from selenium.webdriver.chrome import webdriver
+from time import sleep
+driver=webdriver.WebDriver()
+driver.maximize_window()
+driver.get("https://www.baidu.com")
+print(driver.title)
+sleep(2)
+print("All the test finished")
+driver.quit()
